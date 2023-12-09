8b352d144b2729c26f55a0ad9c54de410ad8153d
CamiloGato <139593241+camilorpoly@users.noreply.github.com>
2023-12-09 01:32:22 -0500

    Finish: Problem ArrayDs

---
+++ ProblemSolving.DataStructures/ArrayDs/ArrayDs.h
@@ -0,0 +1,13 @@
+﻿#ifndef ARRAYDS_H
+#define ARRAYDS_H
+
+#include <vector>
+#include <string>
+
+std::vector<int> reverseArray(std::vector<int> a);
+int arrayDs();
+std::string ltrim(const std::string &str);
+std::string rtrim(const std::string &str);
+std::vector<std::string> split(const std::string &str);
+
+#endif
---
+++ ProblemSolving.DataStructures/ProblemSolving.DataStructures.cpp
@@ -0,0 +1,7 @@
+#include "ArrayDs/ArrayDs.h"
+
+int main(int argc, char* argv[])
+{
+    arrayDs();
+    return 0;
+}
