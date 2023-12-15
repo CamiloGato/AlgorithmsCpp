162fb3e9b1ff460a6ded74390ae6772b0c1dd754
CamiloGato <cm.andres022@hotmail.com>
2023-12-14 22:54:31 -0500

    solved: ReverseLinkedList problem

---
+++ ProblemSolving.DataStructures/ReverseLinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,55 @@
+﻿#include "ReverseLinkedList.h"
+
+#include <iostream>
+
+using namespace std;
+
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* head)
+{
+    SinglyLinkedListNode* prev = nullptr;
+    SinglyLinkedListNode* current = head;
+    SinglyLinkedListNode* next = nullptr;
+
+    while (current != nullptr)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+
+int reverseLinkedList()
+{
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        SinglyLinkedListNode* llist1 = reverse(llist->head);
+
+        vector<int> output;
+        print_singly_linked_list(llist1, " ", output);
+        cout << "\n";
+
+        free_singly_linked_list(llist1);
+    }
+
+    return 0;
+}
---
+++ ProblemSolving.DataStructures/ReverseLinkedList/ReverseLinkedList.h
@@ -0,0 +1,9 @@
+﻿#ifndef REVERSE_LINKED_LIST
+#define REVERSE_LINKED_LIST
+
+#include "../_Common/SinglyLinkedList.h"
+
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* head);
+int reverseLinkedList();
+
+#endif
